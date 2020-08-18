23db520dcb67654641c3025c49f25a26fb8abc12
Sachin Gwasikoti <sachingw777@gmail.com>
2020-08-18 23:40:48 +0530

    add schema.
    User, Site & Comment Schema defined.

---
+++ models/site.js
@@ -0,0 +1,25 @@
+var mongoose    = require("mongoose");
+
+// SCHEMA SETUP
+
+var siteSchema = new mongoose.Schema({
+   name: String,
+   price: String,
+   image: String,
+   description: String,
+   author: {
+      id: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "User"
+      },
+      username: String
+   },
+   comments: [
+      {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Comment"
+      }
+   ]
+});
+
+module.exports = mongoose.model("Site", siteSchema); //creates model with above schema and has methods such as .find etc.
